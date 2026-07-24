df63c3012252aba3f9b10fa067dd71f14039b67a
bexpec-gif <bexpec@gmail.com>
2026-07-24 21:56:26 +0500

    Update Hello.js

--- module4-solution/js/Hello.js
+++ module4-solution/js/Hello.js
@@ -1,15 +1,8 @@
-// IIFE - Immediately Invoked Function Expression
 (function(window) {
-    // STEP 1: Create an object called 'helloSpeaker'
     var helloSpeaker = {};
-
-    // STEP 2: Create a property 'speak' that contains a function
-    // The function takes a 'name' parameter and logs "Hello " + name
     helloSpeaker.speak = function(name) {
         console.log("Hello " + name);
     };
-
-    // STEP 3: Expose the 'helloSpeaker' object to the global scope
     window.helloSpeaker = helloSpeaker;
 
 })(window);
